0e3c91476fc176d65508bf23910d752cd6c07fb4
Hugo Lopes <hugo.lopes.extern@sixt.com>
2023-03-11 15:45:54 +0000

    feat(chore): add alias

--- jest.config.ts
+++ jest.config.ts
@@ -5,5 +5,6 @@ export default {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.ts',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
 }
